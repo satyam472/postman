2a9d636d90f79904dfc7381f8d945678ade9df2c
Satyam Dubey <68489399+satyam472@users.noreply.github.com>
2021-10-26 20:13:23 +0530

    Add files via upload

---
+++ Postmain/contact/nav.php
@@ -0,0 +1,66 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="prism.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <style>
+        #responsePre {
+            max-height: 500px;
+            background-color: white;
+            border : 2px solid rgb(113, 255, 172);
+        }
+        .navbar-brand{
+            color: rgb(179, 195, 209);
+             font-size: 2.25rem !important;
+        }
+        .response{
+            font-size: 35px !important;
+        }
+    </style>
+    <title>PostMaster by Satyam_Dubey!</title>
+</head>
+
+<body>
+    
+      
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container my-3">
+
+                <a class="navbar-brand" href="#">PostMaster</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText"
+                aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarText">
+                <ul class="navbar-nav m-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link f-4" href="#">Home <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item ">
+                        <a class="nav-link f-4" href="#">Pricing <span class="sr-only">(current)</span></a>
+                    </li>
+                   
+                    <li class="nav-item">
+                        <a class="nav-link" href="./contact/index.php">Contact us</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto justify-content-end">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="login.php">Login/Signup</a>
+                    </li>
+                </ul>
+                
+            </div>
+            </div>
+        </nav>
+    </body>
+</head>
+</html>
